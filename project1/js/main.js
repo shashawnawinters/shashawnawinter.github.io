568cee1f65230ddb95e1448f4b58ef9453812363
shashawnawinters <shashawnawinters>
2016-06-22 13:02:39 -0500

    shuffle

--- project1/js/main.js
+++ project1/js/main.js
@@ -5,10 +5,12 @@ var cards         = ['blackrose', 'flower', 'fvl', 'golden', 'nani', 'sequins2']
 var pairs         = cards.concat(cards);
 var chosenCards   = [];
 var cardsToFlip   = [];
+var cardsFlipped  = 0;
 
-console.log(cards);
-
-randomArray(pairs); // need to qrite this function
+console.log(cards, '<--- My cards');
+console.log(pairs, ' <--- My Pairs');
+console.log('Below is the result of randomArray()...');
+console.log(randomArray(pairs)) // need to qrite this function
 
 $('.back').each(function(i, element) {
   $(this).attr('cards', pairs[i]);
@@ -18,10 +20,18 @@ $('.flip-container').click(function() {
   console.log('click working')
 });
 
-function randomArray(pairs) { 
-  for (var i = array.length - 1; i > 0; i--) {
-    var  = Math.floor(Math.random() * (i + 1));
-  }
-  return 'I need to be implemented!';
 
-}
+function randomArray(pairs) { 
+  // 1. Generate a random number for every value in the `pairs` array
+  // 2. Save each random number into an array
+  // 3. Return the array you created
+
+  var randomIndexes = [];
+
+
+  pairs.forEach(function(pair, i) {
+    randomIndexes.push(Math.floor(Math.random() * (i + 1)));
+  });
+
+  return randomIndexes;
+} 
