d379957f7755ded9eb0f47bdd24b3f29d7529a27
shashawnawinters <shashawnawinters>
2016-06-24 10:37:08 -0500

    latest update

--- project1/js/main.js
+++ project1/js/main.js
@@ -1,5 +1,5 @@
 var cards = shuffleIndexes(['blackrose.jpg', 'golden.jpeg', 'flower.jpg', 'fvl.jpg', 'nani.jpg', 'sequins2.jpg', 'blackrose.jpg', 'golden.jpeg', 'flower.jpg', 'fvl.jpg', 'nani.jpg', 'sequins2.jpg']);
-var rowNumber = 0
+var rowNumber = 0;
 
 cards.forEach(function(card, i) {
   if (i === 0 || i % 4 === 0) {
@@ -8,7 +8,7 @@ cards.forEach(function(card, i) {
   }
 
   $('#row' + rowNumber).append('<div class="col-3"><img src="images/Jashton.JPG" width="200" data-card="' + card + '" /></div>');
-})
+});
 
 // 1. Look up accessing data attributes with jQuery
 // 2. Create a click handler for all images  $('#board').on(click, )
@@ -27,8 +27,8 @@ function shuffleIndexes(indexArray) {
 
   return indexArray;
 }
- var imagesClicked = 0
- var imageNames = []
+ var imagesClicked = 0;
+ var imageNames = [];
 
 
 $('#board').on('click', 'img', function(e) {
@@ -40,14 +40,21 @@ $('#board').on('click', 'img', function(e) {
   imageNames.push(imageView) 
   imagesClicked++ 
   if (imagesClicked === 2) {
+    imagesClicked = 0
     if (imageNames[0] === imageNames[1]) {
       console.log("true Clicked twice")
+
     }else if (imageNames[0] != imageNames[1]) {
       console.log("false");
-      
+      imageNames.forEach(function(imageName, i) {
+        $('#board').on('click', function(e) {
+          $(this).html('' + $(this).attr('src', 'images/' + imagesClicked));
+        });
+      });
+        //flip current cards back over
     }
   }
-})
+});
 
 
 
